e19b021315f23ed80ffb69874d54d6f589c80b0a
lanzhiheng <lanzhihengrj@gmail.com>
2020-02-23 20:48:52 +0800

    feat: change database to pg.

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_02_23_122459) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
