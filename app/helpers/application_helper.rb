256f3d97a11b5b05b561258b7d071f135faca1b3
lanzhiheng <lanzhihengrj@gmail.com>
2020-02-23 21:51:20 +0800

    feat: Add simple page and stylesheet.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,66 @@
 module ApplicationHelper
+  def soc
+    [
+      {
+        class: 'fab fa-github',
+        url: 'https://github.com/lanzhiheng'
+      },
+      {
+        class: 'fab fa-weibo',
+        url: 'https://www.weibo.com/u/2174832927'
+      },
+      {
+        class: 'fab fa-twitter',
+        url: 'https://twitter.com/lanzhiheng'
+      },
+      {
+        class: 'far fa-envelope',
+        url: 'mailto:lanzhihengrj@gmail.com'
+      },
+    ]
+  end
+
+  def nav
+    [
+      {
+        title: 'Lastest',
+        url: '/'
+      },
+      {
+        title: 'Blog',
+        url: '/blog'
+      },
+      {
+        title: 'Translation',
+        url: '/translation'
+      },
+      {
+        title: 'About',
+        url: '/about'
+      },
+      {
+        title: 'Contact',
+        url: '/contact'
+      },
+    ]
+  end
+
+  def navbar
+    content_tag(:ul, class: 'nav') do
+      nav.map do |item|
+        content_tag(:li) do
+          link_to(item[:title], item[:url])
+        end
+      end.join.html_safe
+    end
+  end
+
+  def social
+    content_tag(:ul, class: 'social') do
+      soc.map do |item|
+        inner = content_tag(:i, '', class: item[:class]).html_safe
+        link_to(inner, item[:url], target: '_blank')
+      end.join.html_safe
+    end
+  end
 end
