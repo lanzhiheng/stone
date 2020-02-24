f2b5c5f9e52917c663dc3286dedd62f44751f0ff
lanzhiheng <lanzhihengrj@gmail.com>
2020-02-24 16:31:44 +0800

    feat: Add about page and contact page with some test.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :posts
+  get '/blogs/:id', to: 'posts#show', as: 'blog'
+  get '/blogs', to: 'posts#index', as: 'blogs'
+  get '/about', to: 'about#index', as: 'about'
+  get '/contact', to: 'contact#index', as: 'contact'
 end
