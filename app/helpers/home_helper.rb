79ea88c24b2ca788f6dd4097995e73141140239f
lanzhiheng <lanzhihengrj@gmail.com>
2020-02-24 09:46:50 +0800

    feat: Add excerpt for post with some test.

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,5 @@
 module HomeHelper
+  def lastest_posts
+    Post.limit(10).order('created_at desc')
+  end
 end
--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -5,4 +5,20 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
     get root_url
     assert_response :success
   end
+
+  test "should contain home section with post-list" do
+    get root_url
+    assert_select 'div.home' do
+      assert_select 'ul.post-list' do
+        assert_select "li.post-wrapper", 2 do
+          assert_select "a.post-title", 2
+          assert_select "span.post-meta", 2
+          assert_select "p.post-excerpts", 2
+          assert_select "div.btn-wrapper" do
+            assert_select "a.read-more"
+          end
+        end
+      end
+    end
+  end
 end
