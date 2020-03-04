bea9ed46df04904bea75e3909e6ccf9c13776cde
lanzhiheng <lanzhihengrj@gmail.com>
2020-03-05 00:11:10 +0800

    hotfix: Fix timezone issue.

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Stone
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.exceptions_app = routes
+    config.time_zone = "Asia/Shanghai"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
