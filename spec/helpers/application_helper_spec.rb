ba144c1853d72a518750f4b5dfdb122a9cd78269
lanzhiheng <lanzhihengrj@gmail.com>
2020-03-13 18:45:40 +0800

    test: Adjust some test.

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,4 +2,13 @@ require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
 
+  describe "sidebar link" do
+    it "navbar link" do
+      expect(helper.navbar).to have_tag("a", count: 5)
+    end
+
+    it "social link" do
+      expect(helper.social).to have_tag("a", count: 4)
+    end
+  end
 end
--- test/helpers/home_helper_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class HomeHelperTest < ActionView::TestCase
-  test "should contain lastest posts" do
-    assert_equal lastest_posts, Post.published.limit(10).reverse
-  end
-end
