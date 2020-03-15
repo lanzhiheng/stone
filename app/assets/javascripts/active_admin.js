81de710a18f499d7ea0b6732c272988a5351946d
lanzhiheng <lanzhihengrj@gmail.com>
2020-03-15 17:17:31 +0800

    feature: Make softlink for css and javascript file in for easymde.

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -1,2 +1,10 @@
 //= require active_admin/base
+//= require easymde.min.js
 //= require_self
+
+window.addEventListener('DOMContentLoaded', (event) => {
+  var easyMDE = new EasyMDE({
+    element: document.getElementById('md_editor'),
+    spellChecker: false
+  });
+})
