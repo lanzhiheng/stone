95a2fbc159648d6d1cd2197fa365c93a5c19f013
lanzhiheng <lanzhihengrj@gmail.com>
2020-05-13 18:34:25 +0800

    feat: Add selet2 updating.

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -1,10 +1,29 @@
 //= require active_admin/base
 //= require easymde.min.js
+//= require select2/dist/js/select2.js
 //= require_self
 
-window.addEventListener('DOMContentLoaded', (event) => {
+$(document).ready(function() {
   var easyMDE = new EasyMDE({
     element: document.getElementById('md_editor'),
     spellChecker: false
   });
+
+  var tagListInput = $('#post_tag_list').select2({
+    tags: true,
+    data: [
+      {
+        id: 'enhancement',
+        text: 'enhancement'
+      },
+      {
+        id: 'bug',
+        text: 'bug'
+      },
+      {
+        id: 'wontfix',
+        text: 'wontfix'
+      }
+    ]
+  })
 })
