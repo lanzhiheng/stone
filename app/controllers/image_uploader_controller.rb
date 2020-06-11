b05bd8224caa1c517f5bbead8e249b531517cccd
lanzhiheng <lanzhihengrj@gmail.com>
2020-06-11 16:53:07 +0800

    feat: Api for image uploading.

---
+++ app/controllers/image_uploader_controller.rb
@@ -0,0 +1,30 @@
+class ImageUploaderController < ApplicationController
+  def upload
+    file = permitted_params[:file]
+    name = file.original_filename
+    body = file.tempfile
+
+    begin
+      obj = aws_bucket.object(name)
+      obj.put(body: body)
+      render json: { url: url_formatter(obj), name: name }
+    rescue
+      render json: { msg: "upload failed" }, status: 422
+    end
+  end
+
+  private
+  def permitted_params
+    params.permit(:file)
+  end
+
+  def url_formatter(object)
+    region = Aws.config[:region]
+    "https://#{object.bucket_name}.s3-#{region}.amazonaws.com/#{object.key}"
+  end
+
+  def aws_bucket
+    s3 = Aws::S3::Resource.new
+    s3.bucket(Rails.application.credentials.dig(:aws, :bucket))
+  end
+end
