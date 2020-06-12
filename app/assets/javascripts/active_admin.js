9e47696dd7a2da4712491eab6a00ca7c241b4bfe
lanzhiheng <lanzhihengrj@gmail.com>
2020-06-12 11:15:21 +0800

    feat: Add uploading javascript code.

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -1,12 +1,35 @@
 //= require arctic_admin/base
 //= require easymde.min.js
+// To avoid CSRF token authenticity issue https://guides.rubyonrails.org/security.html#cross-site-request-forgery-csrf
+//= require rails-ujs
 //= require select2/dist/js/select2.js
 //= require_self
 
 $(document).ready(function() {
   var easyMDE = new EasyMDE({
     element: document.getElementById('md_editor'),
-    spellChecker: false
+    spellChecker: false,
+    uploadImage: true,
+    imageUploadFunction: function(file) {
+      var formData = new FormData()
+      formData.append('file', file)
+
+      $.ajax({
+        processData: false,
+        contentType: false,
+        url: '/upload',
+        method: 'PUT',
+        data: formData,
+        success: function(data) {
+          const { name, url } = data
+          const textToInsert = `![${name}](${url})`
+          easyMDE.codemirror.replaceSelection(textToInsert)
+        },
+        error: function(data) {
+          alert(data.responseJSON.msg)
+        }
+      })
+    }
   });
 
   $('#post_tag_list').select2({
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+  protect_from_forgery with: :exception
+
+  rescue_from ActionController::InvalidAuthenticityToken do |exception|
+    render json: { msg: "Invalid AuthenticityToken" }, status: 422
+  end
 end
