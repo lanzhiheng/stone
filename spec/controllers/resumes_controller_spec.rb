d75e6144f07def536db145d82af8b520ec90af43
lanzhiheng <lanzhihengrj@gmail.com>
2020-07-14 16:19:54 +0800

    chore: modify some meta data.

--- spec/controllers/resumes_controller_spec.rb
+++ spec/controllers/resumes_controller_spec.rb
@@ -10,4 +10,17 @@ RSpec.describe ResumesController, type: :controller do
     get :index
     expect(response.body).to have_tag('.resume', count: 2)
   end
+
+  it "meta data for resume page" do
+    get :index
+    expect(response.body).to have_tag("title", "About" )
+    expect(response.body).to have_tag("meta", with: {
+                                        name: 'keywords',
+                                        content: DEFAULT_META["keywords"]
+                                      })
+    expect(response.body).to have_tag("meta", with: {
+                                        name: 'description',
+                                        content: 'Simple description of the author.'
+                                      })
+  end
 end
