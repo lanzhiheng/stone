ade8f963eade5d4b7824f82ffa340f8454e757a6
lanzhiheng <lanzhihengrj@gmail.com>
2020-07-23 16:46:14 +0800

    feat: Add test for admin controller.

---
+++ spec/controllers/admin/posts_controller_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Admin::PostsController, type: :controller do
+  render_views
+
+  let(:page) { Capybara::Node::Simple.new(response.body) }
+  let(:user) { create(:admin_user) }
+  before { sign_in user }
+
+  describe 'GET index' do
+    let(:filters_sidebar) { page.find('#filters_sidebar_section') }
+    it 'filter Name exists' do
+      get :index
+      expect(filters_sidebar).to have_css('label[for="q_title_contains"]', text: 'Title')
+      expect(filters_sidebar).to have_css('label[for="q_category_id"]', text: 'Category')
+      expect(filters_sidebar).to have_css('label[for="q_draft_not_true"]', text: 'Published')
+    end
+  end
+end
