0f6cfa6e9da937fb506052297a8abdf509da7289
lanzhiheng <lanzhihengrj@gmail.com>
2020-07-25 18:01:45 +0800

    feat: Add personal resume.

--- app/helpers/resumes_helper.rb
+++ app/helpers/resumes_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module ResumesHelper
+  def range(num)
+    content_tag :div, class: 'wrapper' do
+      (1..5).map do |i|
+        content_tag :i, nil, class: "fa fa-bug #{i <= num ? 'check' : ''}"
+      end.join.html_safe
+    end
+  end
 end
---
+++ spec/helpers/resumes_helper_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe ResumesHelper, type: :helper do
+  it 'range' do
+    expect(range(0)).to have_tag('i.fa.fa-bug', count: 5)
+    expect(range(0)).not_to have_tag('i.check')
+
+    expect(range(4)).to have_tag('i.fa.fa-bug', count: 5)
+    expect(range(4)).to have_tag('i.fa.fa-bug.check', count: 4)
+  end
+end
