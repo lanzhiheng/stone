70c7c25f637c943606346c3161151814f2a057cd
lanzhiheng <lanzhihengrj@gmail.com>
2020-08-09 19:48:05 +0800

    feat: Add unique index for category's key.

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,29 +3,42 @@
 require 'rails_helper'
 
 RSpec.describe Category do
-  context 'create category' do
-    it 'without any fields' do
-      Category.create
-      expect(Category.count).to eq 0
-    end
+  let!(:category) { build(:category) }
 
+  context 'create category' do
     it 'without key' do
+      category.key = nil
       expect do
-        create(:category, key: nil)
+        category.save!
       end.to raise_error(ActiveRecord::RecordInvalid)
-      expect(Category.count).to eq 0
     end
 
     it 'without name' do
+      category.name = nil
       expect do
-        create(:category, name: nil)
+        category.save!
       end.to raise_error(ActiveRecord::RecordInvalid)
-      expect(Category.count).to eq 0
     end
 
     it 'with key and name' do
       create(:category)
       expect(Category.count).to eq 1
+    end
+
+    it 'should not contain duplicated key' do
+      create(:category)
+      expect do
+        create(:category)
+      end.to raise_error(ActiveRecord::RecordInvalid, 'Validation failed: Key has already been taken')
+    end
+
+    it 'key with unique index' do
+      category.save!
+      ca = create(:category, key: 'new_key')
+
+      expect do
+        ca.update_attribute('key', category.key) # skip application validation
+      end.to raise_error(ActiveRecord::RecordNotUnique)
     end
   end
 end
