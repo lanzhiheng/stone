93345cc405d5c44d935c7877104dd647405c7cb8
lanzhiheng <lanzhihengrj@gmail.com>
2020-11-29 23:16:52 +0800

    feat: Change database dump.

--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,6 @@ module Stone
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.active_record.schema_format = :sql
   end
 end
