5234c14456e29c934e8672a4dd03a8ca53d08abe
lanzhiheng <lanzhihengrj@gmail.com>
2021-05-20 01:26:24 +0800

    feat: Add Management page of category and post.

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ module Stone
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.action_view.form_with_generates_remote_forms = false
     config.active_record.schema_format = :sql
     config.autoload_paths << Rails.root.join('lib')
   end
---
+++ spec/requests/musk/categories_request_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe "Musk::Categories", type: :request do
+
+end
