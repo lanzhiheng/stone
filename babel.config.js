0faf838ed5e8112d9a38681fc7d09fd09ec4f6a6
lanzhiheng <lanzhihengrj@gmail.com>
2021-06-06 12:04:56 +0800

    hotfix: Fix online issue.

--- babel.config.js
+++ babel.config.js
@@ -66,6 +66,9 @@ module.exports = function(api) {
         {
           async: false
         }
+      ],
+      [
+        "@babel/plugin-proposal-private-methods", { "loose": true }
       ]
     ].filter(Boolean)
   }
